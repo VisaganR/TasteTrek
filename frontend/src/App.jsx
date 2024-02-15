8de22e322cfe9343e0073777354c504c7da00800
VisaganR <visagan126@gmail.com>
2024-02-15 10:12:42 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './components/Header/Header';
+
+
+
+function App() {
+  // const { showLoading, hideLoading } = useLoading();
+
+
+  return (
+    <>
+    
+      <Header />
+  
+    </>
+  );
+}
+
+export default App;
